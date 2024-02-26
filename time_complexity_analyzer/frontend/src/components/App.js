34652c6c6a950f8e62aa5c832241e67bd534e039
AlidinAbylkasymUulu <alidinabylkasymuulu03@gmail.com>
2024-02-26 23:20:44 +0100

    installing frontend tools and integrating it with django

---
+++ time_complexity_analyzer/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component } from  'react';
+import {render} from  "react-dom";
+
+export default class  App extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <h1>Testing react Code</h1>
+    }
+}
+const appDiv = document.getElementById( "app" );
+render (<App /> , appDiv)
