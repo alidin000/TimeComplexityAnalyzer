0941447e16e6eb1bd50a071b627074d83fb8feb9
Abylkasym uulu Alidin <alidinablylkasymuulu03@gmail.com>
2024-05-27 04:01:39 +0200

    about us page update

--- time_complexity_analyzer/frontend/src/components/AboutUs.js
+++ time_complexity_analyzer/frontend/src/components/AboutUs.js
@@ -36,8 +36,8 @@ const AboutUs = () => {
                 position: 'absolute',
                 top: 0,
                 left: 0,
-                width: '100%',
-                height: '100%'
+                width: '60%',
+                height: '60%'
               }}
             ></iframe>
           </Box>
